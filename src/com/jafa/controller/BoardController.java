bd920014b9f9496bd0fe588eb5b74606692c3d35
jangmingee <ariel123@naver.com>
2023-08-09 12:11:46 +0900

    BoardRepository 추 가

--- src/com/jafa/controller/BoardController.java
+++ src/com/jafa/controller/BoardController.java
@@ -3,6 +3,6 @@ package com.jafa.controller;
 public class BoardController {
 
 	public void list() {
-		System.out.println("리스트");
+		System.out.println("리스트.리스트");
 	}
 }
---
+++ src/com/jafa/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.jafa.repository;
+
+import java.util.List;
+
+public interface BoardRepository {
+
+	List<String> list();
+}
